98498673e2968eb3fb2b2272cff5089cecb81121
ikasam <ikasam.k503@gmail.com>
2019-12-09 03:18:44 +0900

    save

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -1,38 +1,29 @@
 @verbatim
-<div id="app-4">
-    <ol>
-        <li v-for="todo in todos">
-            {{ todo.text }}
-        </li>
-    </ol>
-</div>
-<div id="app-5">
-    <p>{{ message }}</p>
-    <button v-on:click="reverseMessage">Reverse Message</button>
+<div id="app">
+  <p>Original mesage: "{{ message }}"</p>
+  <p>Computed reversed mesage: "{{ reversedMessage }}"</p>
+  <input v-model="message">
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script>
-    var app4 = new Vue({
-        el: '#app-4',
-        data: {
-            todos: [
-            { text: 'Learn JavaScript' },
-            { text: 'Learn Vue' },
-            { text: 'Build something awesome' }
-            ]
-        }
-    })
-    var app5 = new Vue({
-        el: '#app-5',
-        data: {
-            message: 'Hello Vue.js!'
-        },
-        methods: {
-            reverseMessage: function () {
-                this.message = this.message.split('').reverse().join('')
-            }
-        }
-    })
+var obj = {
+  foo: 'bar'
+}
+
+vm = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+Object.freeze(vm.data)
+
 </script>
 @endverbatim
